49b6732aba57b7748046ab8883111b36c910ea7f
Otacilio Lacerda <otaciliolacerda@gmail.com>
2021-02-27 20:12:22 +0100

    Init (#1)
    - Setup NextJS app
    - Setup ESLint
    - Setup lint-staged to run the linter on every commit
    - Setup project to use absolute imports
    - Setup Prettier
    - Hello World

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome</title>
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+}
