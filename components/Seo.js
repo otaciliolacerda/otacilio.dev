a68e2efcada38cdad0ec94bad75ac6fc6e100c85
Otacilio Lacerda <otaciliolacerda@gmail.com>
2021-04-06 22:53:40 +0200

    add full to og:image

--- components/Seo.js
+++ components/Seo.js
@@ -1,26 +1,32 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import { getSiteMetaData } from 'utils/helpers';
 
 function Seo({ title, description = '', preview = '' }) {
+  const router = useRouter();
   const siteMetadata = getSiteMetaData();
 
   const metaTitle = title || siteMetadata.title;
   const metaDescription = description || siteMetadata.description;
-  const metaPreview = preview || siteMetadata.preview;
+  const metaPreview = `https://otacilio.dev/${router.asPath}/${preview || siteMetadata.preview || ''}`;
 
   return (
     <Head>
       <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
+
       <meta property="og:type" content="website" />
-      <meta name="og:title" property="og:title" content={metaTitle} />
-      <meta name="og:description" property="og:description" content={metaDescription} />
-      <meta name="og:image" property="og:image" content={metaPreview} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:image" content={`${metaPreview}`} />
+      <meta property="og:description" content={metaDescription} />
+
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:image" content={`${metaPreview}`} />
       <meta name="twitter:description" content={metaDescription} />
       <meta name="twitter:creator" content={siteMetadata.social.twitter} />
+
       <link rel="icon" type="image/png" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/favicon.ico" />
     </Head>
