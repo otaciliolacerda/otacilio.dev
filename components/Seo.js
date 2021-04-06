e398a03e068ded994c4f45d7e02fbe2986694c0d
Otacilio Lacerda <otaciliolacerda@gmail.com>
2021-04-06 23:58:37 +0200

    fix typo

--- components/Seo.js
+++ components/Seo.js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 
 import { getSiteMetaData } from 'utils/helpers';
 
-function Seo({ title, preview, description }) {
+function Seo({ title, preview, description, metaType = 'website' }) {
   const router = useRouter();
   const siteMetadata = getSiteMetaData();
 
@@ -17,16 +17,16 @@ function Seo({ title, preview, description }) {
       <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
 
-      <meta property="og:type" content="website" />
+      <meta property="og:type" content={metaType} />
       <meta property="og:title" content={metaTitle} />
-      <meta property="og:image" content={metaPreview} />
       <meta property="og:description" content={metaDescription} />
+      <meta property="og:image" content={metaPreview} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={metaTitle} />
-      <meta name="twitter:image" content={metaPreview} />
       <meta name="twitter:description" content={metaDescription} />
       <meta name="twitter:creator" content={siteMetadata.social.twitter} />
+      <meta name="twitter:image" content={metaPreview} />
 
       <link rel="icon" type="image/png" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/favicon.ico" />
