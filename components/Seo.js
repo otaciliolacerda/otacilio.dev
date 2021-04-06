b7931c44f14de7454408462222670bea0b3314c3
Otacilio Lacerda <otaciliolacerda@gmail.com>
2021-04-07 00:27:21 +0200

    test

--- components/Seo.js
+++ components/Seo.js
@@ -18,15 +18,18 @@ function Seo({ title, preview, description, metaType = 'website' }) {
     <Head>
       <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
+
       <meta property="og:type" content={metaType} />
       <meta property="og:title" content={metaTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:image" content={metaPreview} />
+
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={metaTitle} />
       <meta name="twitter:description" content={metaDescription} />
       <meta name="twitter:creator" content={siteMetadata.social.twitter} />
       <meta name="twitter:image" content={metaPreview} />
+
       <link rel="icon" type="image/png" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/favicon.ico" />
     </Head>
