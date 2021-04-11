c9b281af297a9d5e0b0a0af3fd616979f9c65ed6
Otacilio Lacerda <otaciliolacerda@gmail.com>
2021-04-11 17:08:13 +0200

    fix dynamic require

--- components/Image.js
+++ components/Image.js
@@ -4,13 +4,10 @@ function Image({ alt, src, className }) {
   // eslint-disable-next-line no-param-reassign
   src = src.charAt(0) === '/' ? src.substring(1) : src;
 
-  console.log(require(`content/assets/${src}`));
-  console.log(require(`content/assets/${src}`).default);
-
   return (
     <picture className={className}>
-      <source type="image/webp" data-srcset={require(`content/assets/${src}?webp`).default} />
-      <source type="image/png" data-srcset={require(`content/assets/${src}`).default} />
+      <source type="image/webp" data-srcset={require(`content/assets/${src}?webp`)} />
+      <source type="image/png" data-srcset={require(`content/assets/${src}`)} />
       <img className={`lazyload blur ${className}`} alt={alt} src={require(`content/assets/${src}?trace`).trace} />
     </picture>
   );
