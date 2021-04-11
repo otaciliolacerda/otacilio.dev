4dc5b27a6601684875d23579a59fcd1eb1585591
Otacilio Lacerda <otaciliolacerda@gmail.com>
2021-04-11 17:31:03 +0200

    fix image loading

--- components/Image.js
+++ components/Image.js
@@ -4,12 +4,6 @@ function Image({ alt, src, className }) {
   // eslint-disable-next-line no-param-reassign
   src = src.charAt(0) === '/' ? src.substring(1) : src;
 
-  console.log('----------------------------------------------------');
-  console.log('webp: ', require(`content/assets/${src}?webp&url`));
-  console.log('png: ', require(`content/assets/${src}?url`));
-  console.log('lqip: ', require(`content/assets/${src}?trace`));
-  console.log('----------------------------------------------------');
-
   return (
     <picture className={className}>
       <source type="image/webp" data-srcset={require(`content/assets/${src}?webp&url`)} />
