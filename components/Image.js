5bc1ad9adced381c66fd44ac9d289181dacdb163
Otacilio Lacerda <otaciliolacerda@gmail.com>
2021-04-11 17:26:18 +0200

    test

--- components/Image.js
+++ components/Image.js
@@ -5,15 +5,15 @@ function Image({ alt, src, className }) {
   src = src.charAt(0) === '/' ? src.substring(1) : src;
 
   console.log('----------------------------------------------------');
-  console.log('webp: ', require(`content/assets/${src}?webp`));
-  console.log('png: ', require(`content/assets/${src}`));
+  console.log('webp: ', require(`content/assets/${src}?webp&url`));
+  console.log('png: ', require(`content/assets/${src}?url`));
   console.log('lqip: ', require(`content/assets/${src}?trace`));
   console.log('----------------------------------------------------');
 
   return (
     <picture className={className}>
-      <source type="image/webp" data-srcset={require(`content/assets/${src}?webp`)} />
-      <source type="image/png" data-srcset={require(`content/assets/${src}`)} />
+      <source type="image/webp" data-srcset={require(`content/assets/${src}?webp&url`)} />
+      <source type="image/png" data-srcset={require(`content/assets/${src}?url`)} />
       <img className={`lazyload blur ${className}`} alt={alt} src={require(`content/assets/${src}?trace`).trace} />
     </picture>
   );
