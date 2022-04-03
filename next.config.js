82e7619012b1b63463b14d8993ae48b8fb47ca5b
Otacilio Lacerda <otaciliolacerda@gmail.com>
2022-04-03 13:08:49 +0200

    upgrade to nextjs 12

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 const withOptimizedImages = require('next-optimized-images');
 
 module.exports = withOptimizedImages({
-  /* config for next-optimized-images */
-  // your config for other plugins or the general next.js here...
-  removeOriginalExtension: true,
-
-  optimizeImagesInDev: true,
-
-  future: {
-    webpack5: true,
+  images: {
+    disableStaticImages: true,
   },
-
+  removeOriginalExtension: false,
+  optimizeImagesInDev: true,
   reactStrictMode: true,
 });
